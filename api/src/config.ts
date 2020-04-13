6c5247508745e2cebe464053c49abdaa295f7ea6
Simon Knott <info@simonknott.de>
2020-04-13 23:17:33 +0200

    Rm nbsp

--- api/src/config.ts
+++ api/src/config.ts
@@ -10,7 +10,7 @@ type Config = {
   DISABLE_ROTATING: boolean; 
 }
 
-let config: Config | null = null;
+let config: Config | null = null;
 
 const toNumber = (v?: string) => !!v ? +v : undefined;
 
@@ -19,12 +19,12 @@ export const get = (): Config => {
     const { env } = process;
 
     config = {
-      REDIS_HOSTNAME: env.REDIS_HOSTNAME || "",
+      REDIS_HOSTNAME: env.REDIS_HOSTNAME || "",
       REDIS_PORT: toNumber(env.REDIS_PORT) || 6379,
       SECRET_LENGTH: toNumber(env.SECRET_LENGTH) || 96,
-      SECRETS_KEY: env.SECRETS_KEY || "SECRETS",
+      SECRETS_KEY: env.SECRETS_KEY || "SECRETS",
       TOKEN_EXPIRY: toNumber(env.TOKEN_EXPIRY) || 60 * 60,
-      ROTATION_INTERVAL: toNumber(env.ROTATION_INTERVAL) || 60 * 60,
+      ROTATION_INTERVAL: toNumber(env.ROTATION_INTERVAL) || 60 * 60,
       DISABLE_ROTATING: env.DISABLE_ROTATING === "true",
       ROTATE_ON_STARTUP: env.ROTATE_ON_STARTUP === "true",
       ASYMMETRIC_SIGNING: env.ASYMMETRIC_SIGNING === "true",

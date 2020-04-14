6253ed65f025eb31c431a1fb1edd778a9a7299de
Simon Knott <info@simonknott.de>
2020-04-14 14:17:43 +0200

    Import signer to be exported

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,5 +1,6 @@
+import { Signer } from "./Signer";
+
 export type { SignerConfig } from "./Signer";
 export type { KeyValueStorage } from "./KeyValueStorage";
 export type { JWTRepository } from "./JWTRepository";
-
 export default Signer;

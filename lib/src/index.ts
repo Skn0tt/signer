dc48c487ab973a1472520db3e19d661503788b2b
Simon Knott <info@simonknott.de>
2020-04-14 14:13:04 +0200

    Export interesting types

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,3 +1,5 @@
-import { Signer } from "./Signer";
+export type { SignerConfig } from "./Signer";
+export type { KeyValueStorage } from "./KeyValueStorage";
+export type { JWTRepository } from "./JWTRepository";
 
 export default Signer;
